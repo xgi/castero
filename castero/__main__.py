b9397232ad31dc263a87c9c3cb81b337223be7ad
Jake Robertson <jake@faltro.com>
2018-08-15 13:15:14 -0400

    properly update help menu vars when using -h cmdline parameter

--- castero/__main__.py
+++ castero/__main__.py
@@ -10,6 +10,26 @@ from castero.player import Player
 
 
 def main():
+    # instantiate DataFile-based objects
+    config = Config()
+    feeds = Feeds()
+
+    # update fields in help menu text
+    for field in config:
+        if "{%s}" % field in castero.__help__:
+            castero.__help__ = \
+                castero.__help__.replace(
+                    "{%s}" % field,
+                    config[field].ljust(9)
+                )
+        elif "{%s|" % field in castero.__help__:
+            field2 = castero.__help__.split("{%s|" % field)[1].split("}")[0]
+            castero.__help__ = \
+                castero.__help__.replace(
+                    "{%s|%s}" % (field, field2),
+                    ("%s or %s" % (config[field], config[field2])).ljust(9)
+                )
+
     # check if user is running the client with an info flag
     info_flags = {
         'help': ['-h', '--help'],
@@ -24,27 +44,6 @@ def main():
 
     # check whether dependencies are met
     Player.check_dependencies()
-
-    # instantiate DataFile-based objects
-    config = Config()
-    feeds = Feeds()
-
-    # update fields in help menu text
-    for field in config:
-
-        if "{%s}" % field in castero.__help__:
-            castero.__help__ = \
-                castero.__help__.replace(
-                    "{%s}" % field,
-                    config[field].ljust(9)
-                )
-        elif "{%s|" % field in castero.__help__:
-            field2 = castero.__help__.split("{%s|" % field)[1].split("}")[0]
-            castero.__help__ = \
-                castero.__help__.replace(
-                    "{%s|%s}" % (field, field2),
-                    ("%s or %s" % (config[field], config[field2])).ljust(9)
-                )
 
     # instantiate the display object
     stdscr = curses.initscr()
