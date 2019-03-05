720422f6813fd5e66af85c47623365211970cd92
Jake Robertson <jake@faltro.com>
2019-03-05 15:34:09 -0500

    add support for replacing/updating db entries

--- castero/feedsdb.py
+++ castero/feedsdb.py
@@ -23,41 +23,6 @@ class FeedsDB():
             self._cursor.executescript(schema)
             self._migrate_from_old_feeds()
 
-    def feeds(self) -> List[Feed]:
-        sql = "select key, title, description, link, last_build_date, copyright from feed"
-        self._cursor.execute(sql)
-
-        feeds = []
-        for row in self._cursor.fetchall():
-            feeds.append(Feed(
-                url=row[0] if row[0].startswith('http') else None,
-                file=row[0] if not row[0].startswith('http') else None,
-                title=row[1],
-                description=row[2],
-                link=row[3],
-                last_build_date=row[4],
-                copyright=row[5],
-                episodes=[],
-            ))
-        return feeds
-
-    def episodes(self, feed: Feed) -> List[Episode]:
-        sql = "select title, description, link, pubdate, copyright, enclosure from episode where feed=?"
-        self._cursor.execute(sql, (feed.key,))
-
-        episodes = []
-        for row in self._cursor.fetchall():
-            episodes.append(Episode(
-                feed,
-                title=row[0],
-                description=row[1],
-                link=row[2],
-                pubdate=row[3],
-                copyright=row[4],
-                enclosure=row[5]
-            ))
-        return episodes
-
     def _migrate_from_old_feeds(self):
         path = os.path.join(DataFile.DATA_DIR, 'feeds')
 
@@ -93,3 +58,68 @@ class FeedsDB():
                 ))
 
         self._conn.commit()
+
+    def replace_feed(self, feed: Feed) -> None:
+        sql = "replace into feed (key, title, description, link, last_build_date, copyright)\n" \
+              "values (?,?,?,?,?,?)"
+        self._cursor.execute(sql, (
+            feed.key,
+            feed.title,
+            feed.description,
+            feed.link,
+            feed.last_build_date,
+            feed.copyright
+        ))
+        self._conn.commit()
+
+    def replace_episode(self, feed: Feed, episode: Episode) -> None:
+        sql = "replace into episode (key, title, feed, description, link, pubdate, copyright, enclosure)\n" \
+              "values (?,?,?,?,?,?,?,?)"
+        self._cursor.execute(sql, (
+            episode.key,
+            episode.title,
+            feed.key,
+            episode.description,
+            episode.link,
+            episode.pubdate,
+            episode.copyright,
+            episode.enclosure
+        ))
+        self._conn.commit()
+
+    @property
+    def feeds(self) -> List[Feed]:
+        sql = "select key, title, description, link, last_build_date, copyright from feed"
+        self._cursor.execute(sql)
+
+        feeds = []
+        for row in self._cursor.fetchall():
+            feeds.append(Feed(
+                url=row[0] if row[0].startswith('http') else None,
+                file=row[0] if not row[0].startswith('http') else None,
+                title=row[1],
+                description=row[2],
+                link=row[3],
+                last_build_date=row[4],
+                copyright=row[5],
+                episodes=[],
+            ))
+        return feeds
+
+    @property
+    def episodes(self, feed: Feed) -> List[Episode]:
+        sql = "select title, description, link, pubdate, copyright, enclosure from episode where feed=?"
+        self._cursor.execute(sql, (feed.key,))
+
+        episodes = []
+        for row in self._cursor.fetchall():
+            episodes.append(Episode(
+                feed,
+                title=row[0],
+                description=row[1],
+                link=row[2],
+                pubdate=row[3],
+                copyright=row[4],
+                enclosure=row[5]
+            ))
+        return episodes
