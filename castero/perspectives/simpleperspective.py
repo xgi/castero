1b4622758e7249872eaba9a9255fd0b90460c464
Jake Robertson <jake@faltro.com>
2019-03-10 18:00:15 -0400

    add a metadata-less perspective

---
+++ castero/perspectives/simpleperspective.py
@@ -0,0 +1,263 @@
+import curses
+
+from castero import helpers
+from castero.config import Config
+from castero.menu import Menu
+from castero.menus.episodemenu import EpisodeMenu
+from castero.perspective import Perspective
+from castero.player import Player
+
+
+class SimplePerspective(Perspective):
+    """The SimplePerspective class.
+
+    TODO: details here
+    """
+    ID = 3
+
+    def __init__(self, display) -> None:
+        """Initializes the object.
+
+        Overrides method from Perspective; see documentation in that class.
+        """
+        super().__init__(display)
+        self._active_window = 0
+        self._feed_window = None
+        self._episode_window = None
+        self._feed_menu = None
+        self._episode_menu = None
+
+    def create_windows(self) -> None:
+        """Create and set basic parameters for the windows.
+
+        Overrides method from Perspective; see documentation in that class.
+        """
+        # delete old windows if they exist
+        if self._feed_window is not None:
+            del self._feed_window
+            self._feed_window = None
+        if self._episode_window is not None:
+            del self._episode_window
+            self._episode_window = None
+
+        parent_x = self._display.parent_x
+        parent_y = self._display.parent_y
+        
+
+        fw_width = min(25, parent_x / 2)
+
+        self._feed_window = curses.newwin(parent_y - 2, fw_width,
+                                          2, 0)
+        self._episode_window = curses.newwin(parent_y - 2, parent_x - fw_width,
+                                             2, fw_width)
+
+        self._feed_window.attron(curses.color_pair(1))
+        self._episode_window.attron(curses.color_pair(1))
+
+    def create_menus(self) -> None:
+        """Create the menus used in each window, if necessary.
+
+        Overrides method from Perspective; see documentation in that class.
+        """
+        assert all(window is not None for window in [
+            self._feed_window, self._episode_window
+        ])
+
+        # this method could change a lot of screen content - probably
+        # reasonable to simply clear the whole screen
+        self._display.clear()
+
+        # delete old menus if they exist
+        if self._feed_menu is not None:
+            del self._feed_menu
+            self._feed_menu = None
+        if self._episode_menu is not None:
+            del self._episode_menu
+            self._episode_menu = None
+
+        feed_items = [[]]
+        for key in self._display.feeds:
+            feed = self._display.feeds[key]
+            feed_items[0].append(
+                str(feed)
+            )
+
+        self._episode_menu = EpisodeMenu(self._episode_window,
+                                         self._display._feeds)
+        self._feed_menu = Menu(self._feed_window, feed_items,
+                               child=self._episode_menu, active=True)
+
+        # force reset active window to prevent starting in the episodes menu
+        self._active_window = 0
+
+    def display(self) -> None:
+        """Draws all windows and sub-features, including titles and borders.
+
+        Overrides method from Perspective; see documentation in that class.
+        """
+        # add window titles
+        self._feed_window.attron(curses.A_BOLD)
+        self._episode_window.attron(curses.A_BOLD)
+        self._feed_window.addstr(0, 0, "Feeds")
+        self._episode_window.addstr(0, 0, "Episodes")
+
+        # add window borders
+        self._feed_window.hline(1, 0,
+                                0, self._feed_window.getmaxyx()[1])
+        self._episode_window.hline(1, 0,
+                                   0, self._episode_window.getmaxyx()[1])
+        if not helpers.is_true(Config["disable_vertical_borders"]):
+            self._feed_window.vline(0, self._feed_window.getmaxyx()[1] - 1,
+                                    0, self._feed_window.getmaxyx()[0] - 2)
+
+        # display menu content
+        self._feed_menu.display()
+        self._episode_menu.display()
+
+    def handle_input(self, c) -> bool:
+        """Performs action corresponding to the user's input.
+
+        Overrides method from Perspective; see documentation in that class.
+        """
+        queue = self._display.queue
+        key_mapping = self._display.KEY_MAPPING
+
+        keep_running = True
+        if c == key_mapping[Config['key_exit']]:
+            self._display.terminate()
+            keep_running = False
+        elif c == key_mapping[Config['key_help']]:
+            self._display.show_help()
+        elif c == key_mapping[Config['key_right']]:
+            self._change_active_window(1)
+            self._metadata_updated = False
+        elif c == key_mapping[Config['key_left']]:
+            self._change_active_window(-1)
+            self._metadata_updated = False
+        elif c == key_mapping[Config['key_up']]:
+            self._get_active_menu().move(1)
+            self._metadata_updated = False
+        elif c == key_mapping[Config['key_down']]:
+            self._get_active_menu().move(-1)
+            self._metadata_updated = False
+        elif c == key_mapping[Config['key_scroll_up']]:
+            self._get_active_menu().move_page(1)
+            self._metadata_updated = False
+        elif c == key_mapping[Config['key_scroll_down']]:
+            self._get_active_menu().move_page(-1)
+            self._metadata_updated = False
+        elif c == key_mapping[Config['key_play_selected']]:
+            queue.stop()
+            queue.clear()
+            self._create_player_from_selected()
+            queue.play()
+        elif c == key_mapping[Config['key_add_selected']]:
+            self._create_player_from_selected()
+            self._get_active_menu().move(-1)
+        elif c == key_mapping[Config['key_clear']]:
+            queue.stop()
+            queue.clear()
+        elif c == key_mapping[Config['key_pause_play']] or \
+                c == key_mapping[Config['key_pause_play_alt']]:
+            queue.toggle()
+        elif c == key_mapping[Config['key_next']]:
+            queue.stop()
+            queue.next()
+            queue.play()
+        elif c == key_mapping[Config['key_seek_forward']] or \
+                c == key_mapping[Config['key_seek_forward_alt']]:
+            queue.seek(1)
+        elif c == key_mapping[Config['key_seek_backward']] or \
+                c == key_mapping[Config['key_seek_backward_alt']]:
+            queue.seek(-1)
+        elif c == key_mapping[Config['key_add_feed']]:
+            self._display.add_feed()
+        elif c == key_mapping[Config['key_delete']]:
+            if self._active_window == 0:
+                self._display.delete_feed(self._feed_menu.selected_index)
+        elif c == key_mapping[Config['key_reload']]:
+            self._display.reload_feeds()
+        elif c == key_mapping[Config['key_save']]:
+            feed_index = self._feed_menu.selected_index
+            episode_index = self._episode_menu.selected_index if \
+                self._active_window == 1 else None
+            self._display.save_episodes(feed_index, episode_index)
+        elif c == key_mapping[Config['key_invert']]:
+            self._invert_selected_menu()
+
+        return keep_running
+
+    def made_active(self) -> None:
+        """Called each time the perspective is made active (switched to).
+
+        Overrides method from Perspective; see documentation in that class.
+        """
+
+    def refresh(self) -> None:
+        """Refresh the screen and all windows.
+
+        Overrides method from Perspective; see documentation in that class.
+        """
+        self._feed_window.refresh()
+        self._episode_window.refresh()
+
+    def _get_active_menu(self) -> Menu:
+        """Retrieve the active Menu, if there is one.
+
+        Overrides method from Perspective; see documentation in that class.
+        """
+        assert 0 <= self._active_window < 2
+
+        return {
+            0: self._feed_menu,
+            1: self._episode_menu,
+        }.get(self._active_window)
+
+    def _create_player_from_selected(self) -> None:
+        """Creates player(s) based on the selected items and adds to the queue.
+
+        If the active menu is the feed menu, then this will create players for
+        all episodes in the selected feed. If the active menu is the episode
+        menu, this will simply create a single player.
+
+        This method will not clear the queue prior to adding the new player(s),
+        nor will it play the episodes after running.
+        """
+        feed_index = self._feed_menu.selected_index
+        feed = self._display.feeds.at(feed_index)
+        if self._active_window == 0:
+            if feed is not None:
+                for episode in feed.episodes:
+                    player = Player.create_instance(
+                        self._display.AVAILABLE_PLAYERS,
+                        str(episode),
+                        episode.get_playable(),
+                        episode)
+                    self._display.queue.add(player)
+        elif self._active_window == 1:
+            episode_index = self._episode_menu.selected_index
+            if feed is not None:
+                episode = feed.episodes[episode_index]
+                player = Player.create_instance(
+                    self._display.AVAILABLE_PLAYERS,
+                    str(episode),
+                    episode.get_playable(),
+                    episode)
+                self._display.queue.add(player)
+
+    def _invert_selected_menu(self) -> None:
+        """Inverts the contents of the selected menu.
+        """
+        feed_index = self._feed_menu.selected_index
+        if self._active_window == 0:
+            self._display.feeds.sort(toggle_invert=True)
+            self.create_menus()
+        elif self._active_window == 1:
+            feed = self._display.feeds.at(feed_index)
+            if feed is not None:
+                feed.invert_episodes()
+                self._display.feeds.write()
+                self.create_menus()
+                for i in range(feed_index):
+                    self._feed_menu.move(-1)
+                self._change_active_window(1)
---
+++ tests/test_perspective_simple.py
@@ -0,0 +1,153 @@
+import os
+from unittest import mock
+
+from castero.config import Config
+from castero.episode import Episode
+from castero.feed import Feed
+
+my_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+def get_simple_perspective(display):
+    """Retrieve the Simple perspective.
+
+    Args:
+        display: the display containing the loaded perspective
+
+    Returns:
+        Simple: the loaded Simple perspective
+    """
+    display._active_perspective = 3
+    return display.perspectives[3]
+
+
+def test_perspective_simple_borders(display):
+    perspective = get_simple_perspective(display)
+
+    display.display()
+    assert perspective._feed_window.hline.call_count == 1
+    assert perspective._feed_window.vline.call_count == 1
+    assert perspective._episode_window.hline.call_count == 1
+    display._stdscr.reset_mock()
+
+
+def test_perspective_simple_input_keys(display):
+    perspective = get_simple_perspective(display)
+
+    display._footer_window.getch = mock.MagicMock(return_value=10)
+
+    ret_val = perspective.handle_input(ord('q'))
+    assert not ret_val
+    display._stdscr.reset_mock()
+
+    ret_val = perspective.handle_input(ord('h'))
+    assert ret_val
+    display._stdscr.timeout.assert_any_call(-1)
+    display._stdscr.reset_mock()
+
+    movement_keys = [
+        display.KEY_MAPPING[Config['key_up']],
+        display.KEY_MAPPING[Config['key_right']],
+        display.KEY_MAPPING[Config['key_down']],
+        display.KEY_MAPPING[Config['key_left']],
+        display.KEY_MAPPING[Config['key_scroll_up']],
+        display.KEY_MAPPING[Config['key_scroll_down']],
+    ]
+    for key in movement_keys:
+        perspective._metadata_updated = True
+        ret_val = perspective.handle_input(key)
+        assert ret_val
+
+    operation_keys = [
+        display.KEY_MAPPING[Config['key_add_feed']],
+        display.KEY_MAPPING[Config['key_delete']],
+        display.KEY_MAPPING[Config['key_reload']],
+        display.KEY_MAPPING[Config['key_save']],
+        display.KEY_MAPPING[Config['key_play_selected']],
+        display.KEY_MAPPING[Config['key_add_selected']],
+        display.KEY_MAPPING[Config['key_clear']],
+        display.KEY_MAPPING[Config['key_next']],
+        display.KEY_MAPPING[Config['key_invert']],
+        display.KEY_MAPPING[Config['key_pause_play']],
+        display.KEY_MAPPING[Config['key_pause_play_alt']],
+        display.KEY_MAPPING[Config['key_seek_forward']],
+        display.KEY_MAPPING[Config['key_seek_forward_alt']],
+        display.KEY_MAPPING[Config['key_seek_backward']],
+        display.KEY_MAPPING[Config['key_seek_backward_alt']],
+    ]
+    for key in operation_keys:
+        ret_val = perspective.handle_input(key)
+        assert ret_val
+
+
+
+def test_perspective_simple_get_active_menu(display):
+    perspective = get_simple_perspective(display)
+
+    perspective._active_window = 0
+    assert perspective._get_active_menu() == perspective._feed_menu
+    perspective._active_window = 1
+    assert perspective._get_active_menu() == perspective._episode_menu
+
+
+def test_perspective_simple_create_player(display):
+    perspective = get_simple_perspective(display)
+
+    feed = Feed(url="feed url",
+                title="feed title",
+                description="feed description",
+                link="feed link",
+                last_build_date="feed last_build_date",
+                copyright="feed copyright",
+                episodes=[])
+    episode = Episode(feed,
+                      title="episode title",
+                      description="episode description",
+                      link="episode link",
+                      pubdate="episode pubdate",
+                      copyright="episode copyright",
+                      enclosure="episode enclosure")
+    feed.episodes.append(episode)
+    feed.episodes.append(episode)
+    feed.episodes.append(episode)
+    display.feeds["feed url"] = feed
+    perspective._active_window = 0
+    perspective._create_player_from_selected()
+    assert display.queue.length == 3
+    display.queue.clear()
+    assert display.queue.length == 0
+    perspective._active_window = 1
+    perspective._create_player_from_selected()
+    assert display.queue.length == 1
+
+
+def test_perspective_regression_11(display):
+    perspective = get_simple_perspective(display)
+
+    old_ddir = Config["custom_download_dir"]
+    Config.data["custom_download_dir"] = os.path.join(my_dir, "downloaded")
+
+    feed = Feed(file="%s/feeds/valid_enclosures.xml" % (my_dir))
+    display.feeds["feed url"] = feed
+    display.queue.clear()
+    perspective._active_window = 1
+    perspective._create_player_from_selected()
+
+    assert display.queue[0]._path == os.path.join(my_dir, "downloaded",
+                                                  "myfeed_title",
+                                                  "myfeed_item1_title.mp3")
+
+    Config.data["custom_download_dir"] = old_ddir
+
+
+def test_perspective_simple_invert_episodes(display):
+    perspective = get_simple_perspective(display)
+    perspective._active_window = 1
+
+    feed = Feed(file=my_dir + "/feeds/valid_basic.xml")
+    display.feeds["feed url"] = feed
+
+    display.feeds.at(0).invert_episodes = mock.MagicMock()
+    perspective._invert_selected_menu()
+    display.feeds.at(0).invert_episodes.assert_called_once()
+    display._stdscr.reset_mock()
