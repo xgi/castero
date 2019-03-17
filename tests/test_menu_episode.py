245df56bf1a4c002cbb1eb5dcb40b18291d67a7b
Jake Robertson <jake@faltro.com>
2019-03-17 11:43:07 -0400

    add tests for EpisodeMenu

---
+++ tests/test_menu_episode.py
@@ -0,0 +1,78 @@
+import os
+from unittest import mock
+
+import curses
+from castero.config import Config
+from castero.episode import Episode
+from castero.feed import Feed
+from castero.menus.episodemenu import EpisodeMenu
+
+feed = mock.MagicMock(spec=Feed)
+episode1 = mock.MagicMock(spec=Episode)
+episode2 = mock.MagicMock(spec=Episode)
+window = mock.MagicMock()
+window.getmaxyx = mock.MagicMock(return_value=(40, 80))
+source = mock.MagicMock()
+source.episodes = mock.MagicMock(return_value=[episode1, episode2])
+
+
+def test_menu_episode_init():
+    mymenu = EpisodeMenu(window, source)
+    assert isinstance(mymenu, EpisodeMenu)
+
+
+@mock.patch('curses.color_pair')
+@mock.patch('curses.A_NORMAL')
+def test_menu_episode_update_items(mock_color_pair, mock_A_NORMAL):
+    mymenu = EpisodeMenu(window, source)
+    mymenu.update_items(feed)
+    source.episodes.assert_called_with(feed)
+    assert len(mymenu._items()) == 2
+    assert len(mymenu) == 2
+
+
+@mock.patch('curses.color_pair')
+@mock.patch('curses.A_NORMAL')
+def test_menu_episode_items(mock_A_NORMAL, mock_color_pair):
+    mymenu = EpisodeMenu(window, source)
+    mymenu.update_items(feed)
+    items = mymenu._items()
+    assert {
+        'attr': mock_color_pair(5),
+        'text': str(episode1)
+    } in items
+
+
+@mock.patch('curses.color_pair')
+@mock.patch('curses.A_NORMAL')
+def test_menu_episode_item(mock_A_NORMAL, mock_color_pair):
+    mymenu = EpisodeMenu(window, source)
+    mymenu.update_items(feed)
+    assert mymenu.item() == episode1
+    mymenu._selected += 1
+    assert mymenu.item() == episode2
+
+
+@mock.patch('curses.color_pair')
+@mock.patch('curses.A_NORMAL')
+def test_menu_episode_metadata_none(mock_A_NORMAL, mock_color_pair):
+    mymenu = EpisodeMenu(window, source)
+    assert mymenu.metadata() == ""
+    mymenu.update_items(None)
+    assert mymenu.metadata() == ""
+
+
+@mock.patch('curses.color_pair')
+@mock.patch('curses.A_NORMAL')
+def test_menu_episode_metadata(mock_A_NORMAL, mock_color_pair):
+    mymenu = EpisodeMenu(window, source)
+    mymenu.update_items(feed)
+    assert mymenu.metadata() == episode1.metadata
+
+
+@mock.patch('curses.color_pair')
+@mock.patch('curses.A_NORMAL')
+def test_menu_episode_invert(mock_A_NORMAL, mock_color_pair):
+    mymenu = EpisodeMenu(window, source)
+    mymenu.invert()
+    assert mymenu._inverted
